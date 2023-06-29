c3253fdcb4192d968bafb558c820c6c872aef14d
kaviArasu15 <134060086+kaviArasu15@users.noreply.github.com>
2023-06-29 10:32:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function Myage(age) {
+    console.log("my age is "+age);
+}
+function Myname(name) {
+    console.log("my name is "+name);
+}
+function callback(head) {
+    head("kavi")
+   
+}
+callback(Myname)
+
